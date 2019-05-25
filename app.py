6ef513438d2f7a5658389b1559bff92cde60f4b3
cybaj <cybaj@naver.com>
2019-05-26 03:11:41 +0900

    [edge 업데이트] 추가

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from flask import Flask, request
 import konlpy
 from konlpy.tag import Hannanum
 import jpype
+import requests
+import json
+from itertools import combinations
 
 app = Flask(__name__)
 hannanum = Hannanum()
+dbapi_URL = "http://localhost:5000/graph/"
+headers = {'Content-Type': 'application/json; charset=utf-8'} 
 
 @app.route('/')
 def hello_world():
@@ -19,6 +24,22 @@ def noun_extract(uuid):
     
     extracted_collection['han'] = hannanum.nouns(content['sent'])
     print(extracted_collection)
+
+    URL_insert_node = dbapi_URL + "insert/node/"
+    
+    for nodename in extracted_collection['han'] :
+        print(
+            requests.post(URL_insert_node, headers=headers, data=json.dumps({'name': nodename}))
+                    )
+    edgesets = list(combinations(extracted_collection['han'], 2))
+
+    URL_insert_edge = dbapi_URL + "insert/edge/"
+
+    for edgeset in edgesets :
+        print(
+            requests.post(URL_insert_edge, headers=headers, data=json.dumps({'firstNodeName': edgeset[0], 'secondNodeName': edgeset[1]}))
+            )
+
     return 'Success'
 
 if __name__ == '__main__':
